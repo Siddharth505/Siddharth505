695f677d10f61105e3a7470be53e4150cec89bac
Siddharth <Siddharth@LAPTOP-CEPQKL4D>
2021-12-07 11:15:02 +0530

    2. Github Commit

---
+++ src/_1_Scripts/TestingGithubCommit.java
@@ -0,0 +1,14 @@
+package _1_Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestingGithubCommit {
+	
+	@Test
+	public void Testin()
+	{
+		Reporter.log("Testing");
+	}
+
+}
